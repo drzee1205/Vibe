a2eae055612fe46a29c388e6c039d29931fe54e0
awais512 <raza.awais25@gmail.com>
2025-07-10 23:03:41 +0100

    Added prefetching

--- src/app/projects/[projectId]/page.tsx
+++ src/app/projects/[projectId]/page.tsx
@@ -1,3 +1,7 @@
+import { ProjectView } from "@/modules/projects/ui/views/project-view";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+
 import React from "react";
 
 interface Props {
@@ -7,7 +11,21 @@ interface Props {
 const Page = async ({ params }: Props) => {
   const { projectId } = await params;
 
-  return <div>Project ID: {projectId}</div>;
+  const queryClient = getQueryClient();
+
+  void queryClient.prefetchQuery(
+    trpc.messages.getMany.queryOptions({ projectId })
+  );
+
+  void queryClient.prefetchQuery(
+    trpc.projects.getOne.queryOptions({ id: projectId })
+  );
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <ProjectView projectId={projectId} />
+    </HydrationBoundary>
+  );
 };
 
 export default Page;
