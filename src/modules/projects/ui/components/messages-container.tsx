c584913ccd53e3f372f06605f23c1ced99916606
awais512 <raza.awais25@gmail.com>
2025-07-14 22:45:25 +0100

    Add4d messages ui complete

--- src/modules/projects/ui/components/messages-container.tsx
+++ src/modules/projects/ui/components/messages-container.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { MessageCard } from "./message-card";
 import { MessageForm } from "./message-form";
 
@@ -10,10 +10,25 @@ interface Props {
 }
 
 export const MessagesContainer = ({ projectId }: Props) => {
+  const bottomRef = useRef<HTMLDivElement>(null);
   const trpc = useTRPC();
   const { data: messages } = useSuspenseQuery(
     trpc.messages.getMany.queryOptions({ projectId: projectId })
   );
+
+  useEffect(() => {
+    const lastAssistantMessage = messages.findLast(
+      (message) => message.role === "ASSISTANT"
+    );
+
+    if (lastAssistantMessage) {
+    }
+  }, [messages]);
+
+  useEffect(() => {
+    bottomRef.current?.scrollIntoView();
+  }, [messages.length]);
+
   return (
     <div className="flex flex-col flex-1 min-h-0">
       <div className="flex-1 min-h-0 overflow-y-auto">
@@ -30,9 +45,11 @@ export const MessagesContainer = ({ projectId }: Props) => {
               type={message.type}
             />
           ))}
+          <div ref={bottomRef} />
         </div>
       </div>
       <div className="relative p-3 pt-1">
+        <div className="absolute -top-6 left-0 right-0 h-6 bg-gradient-to-b from-transparent to-background/70 pointer-events-none" />
         <MessageForm projectId={projectId} />
       </div>
     </div>
